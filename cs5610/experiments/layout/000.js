ca867c652c58cc59387021f6187ebad934b33320
Jose Annunziato <jannunzi@gmail.com>
2015-02-23 05:23:36 -0500

    wam

---
+++ cs5610/experiments/layout/000.js
@@ -0,0 +1,96 @@
+﻿var app = angular.module("LayoutApp", []);
+
+app.controller("LayoutController", function ($scope) {
+    $scope.layout = {
+        rows: [
+            {
+                cols: [
+                    { content: "C11", span: 4 },
+                    { content: "C12", span: 4 },
+                    { content: "C13", span: 4 },
+                ]
+            }
+        ]
+    };
+    $scope.addRow = function()
+    {
+        var newRow =
+        {
+            cols: [
+                { content: "C"+$scope.layout.rows.length, span: 4 },
+                { content: "C12", span: 4 },
+                { content: "C13", span: 4 }
+            ]
+        };
+
+        $scope.layout.rows.push(newRow);
+    }
+
+    $scope.removeCol = function(cols, index)
+    {
+        cols.splice(index, 1);
+    }
+
+    $scope.widenCol = function (col) {
+        col.span++;
+    }
+
+    $scope.shrinkCol = function (col) {
+        col.span--;
+    }
+
+    var selectedRow = null;
+    var selectedRowIndex = null;
+    var selectedCol = null;
+
+    $scope.select = function (row) {
+        selectedRow = row;
+    }
+    $scope.addColumn = function(row)
+    {
+        var newCol = { content: "New", span: 2 };
+        row.cols.push(newCol);
+    }
+    $scope.removeRow = function(index)
+    {
+        $scope.layout.rows.splice(index, 1);
+    }
+    $scope.moveRowUp = function (index) {
+        if (index > 0)
+        {
+            $scope.layout.rows.move(index, index - 1);
+        }
+    }
+    $scope.moveRowDown = function (index) {
+        if (index < $scope.layout.rows.length - 1) {
+            $scope.layout.rows.move(index, index + 1);
+        }
+    }
+    $scope.hoverIn = function (col) {
+        col.edit = true;
+    }
+    $scope.hoverOut = function (col) {
+        col.edit = false;
+    }
+});
+
+Array.prototype.move = function (old_index, new_index) {
+    if (new_index >= this.length) {
+        var k = new_index - this.length;
+        while ((k--) + 1) {
+            this.push(undefined);
+        }
+    }
+    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
+    return this; // for testing purposes
+};
+
+/*
+            {
+                cols: [
+                    { content: "C11", span: "col-xs-4" },
+                    { content: "C12", span: "col-xs-4" },
+                    { content: "C13", span: "col-xs-4" },
+                ]
+            }
+*/
---
+++ wam/forms.js
@@ -0,0 +1,72 @@
+﻿var app = angular.module("FormsApp", []);
+
+app.controller("FormsController", function ($scope) {
+    $scope.form = {
+        id: "123",
+        name: "Person",
+        buttons: [
+            { label: "Save", type: "save", clas: "btn-primary" },
+            { label: "Cancel", type: "submit", clas: "btn-danger" },
+            { label: "Ok", type: "submit", clas: "btn-success" }
+        ],
+        fields : [
+            { name: "firstName", label: "First Name", type: "text", placeholder:"First Name" },
+            { name: "lastName", label: "Last Name", type: "text", placeholder:"Last Name" },
+            { name: "email", label: "Email", type: "email", placeholder:"someone@somewhere.com" },
+            { name: "dateOfBirth", label: "Date of Birth", type: "date" },
+            {
+                name: "favoriteGenre", label: "Favorite", type: "select",
+                options: [
+                    { value: "HORROR", label: "Horror" },
+                    { value: "COMEDY", label: "Comedy" }
+                ]
+            },
+            {
+                name: "gender", label: "Gender", type: "radios",
+                options: [
+                    { value: "MALE", label: "Male" },
+                    { value: "FEMALE", label: "Female" }
+                ]
+            },
+            {
+                name: "favoriteSports", label: "Favorite Sports", type: "checkboxes",
+                options: [
+                    { value: "BASEBALL", label: "Baseball" },
+                    { value: "FOOTBALL", label: "Footbal" }
+                ]
+            }
+        ]
+    };
+
+    $scope.buttonClick = function(button)
+    {
+        console.log($scope.form);
+//        console.log(button);
+    }
+
+    $scope.removeField = function(index)
+    {
+        $scope.form.fields.splice(index, 1);
+    }
+});
+
+Array.prototype.move = function (old_index, new_index) {
+    if (new_index >= this.length) {
+        var k = new_index - this.length;
+        while ((k--) + 1) {
+            this.push(undefined);
+        }
+    }
+    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
+    return this; // for testing purposes
+};
+
+/*
+            {
+                cols: [
+                    { content: "C11", span: "col-xs-4" },
+                    { content: "C12", span: "col-xs-4" },
+                    { content: "C13", span: "col-xs-4" },
+                ]
+            }
+*/
